219236fc734b711f5371bbba7cab4d7699004c1a
cassielbr <cassielaraujodemelo06@gmail.com>
2024-10-01 14:52:04 -0300

    Initial commit

---
+++ app/src/main/java/br/ulbra/trocatela/MainActivity.java
@@ -0,0 +1,24 @@
+package br.ulbra.trocatela;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnTela = findViewById(R.id.btnTela);
+        btnTela.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
